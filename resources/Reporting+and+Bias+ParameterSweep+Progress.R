611a4297ccd02c9a2ad4df7e73e331a2497a0f3f
Aydin Mohseni <aydin.mohseni@gmail.com>
2020-04-25 16:46:01 -0700

    Newest parameter sweep completed
    Newest parameter sweep completed

--- resources/Reporting+and+Bias+ParameterSweep+Progress.R
+++ resources/Reporting+and+Bias+ParameterSweep+Progress.R
@@ -12,6 +12,7 @@ library(Bolstad)
 #setwd("/Users/patience/Desktop/Images/Absolute2")
 
 # Determine parameter values to test
+Runs <- 100 # The number of runs of the simulation
 TrueStateSD <- 1
 TrueStateMeanVEC <- seq(from = -1, to = 1, by = 1)
 BiasVEC <- seq(from = -1, to = 1, by = 1)
@@ -19,7 +20,7 @@ BiasStrengthVEC <- c(10 ^ -1, 1)
 HyperboleVEC <- seq(from = 1, to = 2, by = 1)
 ExtermityBiasVEC <- seq(from = 0, to = 1, by = 1)
 FairAndBalancedVEC <- c(0, 1)
-quantityOfEvidence <- 3
+QuantityOfEvidence <- 3 
 
 # Calculate the number of distinct parameter settings required
 TotalDataPoints <-
@@ -29,7 +30,7 @@ SampleSize <- 10000
 # Create the data frame in which to save the data
 Df <- data.frame(matrix(
   data = NA,
-  ncol = 8,
+  ncol = 12,
   nrow = TotalDataPoints,
   byrow = TRUE
 ))
@@ -42,7 +43,11 @@ colnames(Df) <-
     "ExtermityBias",
     "FaB",
     "BeliefMean",
-    "BeliefVar"
+    "BeliefVar",
+    "BeliefMeanError",
+    "BeliefVarError",
+    "BeliefMeanMSE",
+    "BeliefVarMSE"
   )
 Df[, 1] <- rep(TrueStateMeanVEC,
                times = 1,
@@ -123,13 +128,6 @@ for (i in 1:TotalDataPoints) {
   priorMean <- Bias
   priorSD <- TrueStateSD
   
-  # Sample (10^q) reports from the News Distribution to be observed by the agent
-  SampleOfNewsReports <-
-    sample(x,
-           10 ^ (quantityOfEvidence),
-           prob = NewsDistribution,
-           replace = T)
-  
   # The Bayesian update function which describes how agents learn from news reports
   # It take reports and prior beliefs about the mean and SD as inputs,
   # and has posterior beliefs aobut the population mean and SD as outputs.
@@ -157,62 +155,81 @@ for (i in 1:TotalDataPoints) {
     return(c(posteriorMean, posteriorSD))
   }
 
-  # And create a vector in which to store the means and SDs of the belief state
-  priorMeanVector <- rep(0, length(SampleOfNewsReports))
-  priorSDVector <- rep(0, length(SampleOfNewsReports))
-  acceptRejectVector <- rep(0, length(SampleOfNewsReports))
+  # Create a vector in which to save the outcomes of each run of the learning process
+  meanPerceptionVec <- rep(NA, Runs)
+  sdPerceptionVec <- rep(NA, Runs)
   
-  # Given our updating function, individiual learning now proceeds as follows:
-  # For each news report, the agent (1) decides whether to accept or reject it
-  # which is determined by how close it is to her current belief.
-  # (2) If she rejects the report, then her view remains unchanged.
-  # If she accepts the report, then she updates her beliefs via Bayes rule.
-  # (3) The process begins again with a new report and her new beliefs.
-  for (obs in 1:length(SampleOfNewsReports)) {
-    # Consider the news report
-    report <- SampleOfNewsReports[obs]
-    # And (1) Decide whether confirmation bias will allow you to update
-    # If confirmation bias makes it so that the report is rejected,
-    # then simply leave the belief state as is
-    if (abs(priorMean - report) > abs(priorMean - rnorm(1, mean = priorMean, BiasStrength ^ -1))) {
-      # Record that the report was rejected
-      acceptRejectVector[obs] <- 0
-      # Leave belief state as is
-      updatedParams <- c(priorMean, priorSD)
-    } else {
-      # If conformation bias does not make it so that the report is rejected,
-      # record that the report was rejected,
-      acceptRejectVector[obs] <- 1
-      # then update the belief state via Bayes' rule.
-      updatedParams <- update(priorMean, priorSD, report)
+  # Run the learning process 'Runs'-many times
+  for (r in 1:Runs) {
+    # Sample (10^q) new reports from the News Distribution to be observed by the agent
+    SampleOfNewsReports <-
+      sample(x,
+             10 ^ (QuantityOfEvidence),
+             prob = NewsDistribution,
+             replace = T)
+    
+    # And create a vector in which to store the means and SDs of the belief state
+    priorMeanVector <- rep(0, length(SampleOfNewsReports))
+    priorSDVector <- rep(0, length(SampleOfNewsReports))
+    acceptRejectVector <- rep(0, length(SampleOfNewsReports))
+    
+    # Given our updating function, individiual learning now proceeds as follows:
+    # For each news report, the agent (1) decides whether to accept or reject it
+    # which is determined by how close it is to her current belief.
+    # (2) If she rejects the report, then her view remains unchanged.
+    # If she accepts the report, then she updates her beliefs via Bayes rule.
+    # (3) The process begins again with a new report and her new beliefs.
+    for (obs in 1:length(SampleOfNewsReports)) {
+      # Consider the news report
+      report <- SampleOfNewsReports[obs]
+      # And (1) Decide whether confirmation bias will allow you to update
+      # If confirmation bias makes it so that the report is rejected,
+      # then simply leave the belief state as is
+      if (abs(priorMean - report) > abs(priorMean - rnorm(1, mean = priorMean, BiasStrength ^ -1))) {
+        # Record that the report was rejected
+        acceptRejectVector[obs] <- 0
+        # Leave belief state as is
+        updatedParams <- c(priorMean, priorSD)
+      } else {
+        # If conformation bias does not make it so that the report is rejected,
+        # record that the report was rejected,
+        acceptRejectVector[obs] <- 1
+        # then update the belief state via Bayes' rule.
+        updatedParams <- update(priorMean, priorSD, report)
+      }
+      # Update the belief means and SD for the next round of learning
+      priorMean <- updatedParams[1]
+      priorSD <- updatedParams[2]
+      # Store thes in their respective vectors
+      priorMeanVector[obs] <- priorMean
+      priorSDVector[obs] <- priorSD
     }
-    # Update the belief means and SD for the next round of learning
-    priorMean <- updatedParams[1]
-    priorSD <- updatedParams[2]
-    # Store thes in their respective vectors
-    priorMeanVector[obs] <- priorMean
-    priorSDVector[obs] <- priorSD
+    # Determine which reports were accepted with whcih to calculate the Bayesian estimator of the SD of the accpeted reports
+    whichReportsAccepted <- which(acceptRejectVector %in% c(1))
+    
+    # Record the posterior mean and SD of individual belief
+    meanPerception <- priorMeanVector[length(SampleOfNewsReports)]
+    sdPerception <- sd(SampleOfNewsReports[whichReportsAccepted])
+    
+    # Save the beliefs to the data vector
+    meanPerceptionVec[r] <- meanPerception
+    sdPerceptionVec[r] <- sdPerception
   }
-  # Determine which reports were accepted with whcih to calculate the Bayesian estimator of the SD of the accpeted reports
-  whichReportsAccepted <- which(acceptRejectVector %in% c(1))
-  
-  # Record the posterior mean and SD of individual belief
-  meanPerception <- priorMeanVector[length(SampleOfNewsReports)]
-  sdPerception <- sd(SampleOfNewsReports[whichReportsAccepted])
-  
-  # Save the beliefs to the data frame
-  Df[i, 7] <- meanPerception
-  Df[i, 8] <- sdPerception
+  # Save the average of the runs inot the data frame
+  Df[i, 7] <- mean(meanPerceptionVec)
+  Df[i, 8] <- mean(sdPerceptionVec)
 }
 
-# Correct Df for error
-Df$BeliefMean <- (Df$BeliefMean - TrueStateMean)
-Df$BeliefVar <- (Df$BeliefVar - TrueStateSD)
+# Calculate the errors in belief given the true states and agent beliefs
+Df$BeliefMeanError <- (Df$BeliefMean - Df$TrueMean)
+Df$BeliefVarError <- (Df$BeliefVar - TrueStateSD)
+Df$BeliefMeanMSE <- Df$BeliefMeanError ^ 2
+Df$BeliefVarMSE <- Df$BeliefVarError ^ 2
 
 # Print all the results as a CSV file
 write.csv(Df, file = "AllData.csv", row.names = FALSE)
 
-# Plot Combinations of Parameters
+# Plot MSE for means for each combinations of parameters
 for (i in 1:length(TrueStateMeanVEC)) {
   Mu <- TrueStateMeanVEC[i]
   for (j in 1:length(BiasVEC)) {
@@ -223,13 +240,80 @@ for (i in 1:length(TrueStateMeanVEC)) {
         FaB <- FairAndBalancedVEC[l]
         # Having set a given combinatio of parameters,
         # open a PDF in which to save the plot
-        pdf(paste("Mu=", Mu, "+b=", b, "+s=", s, "+FaB=", FaB, ".pdf", sep = ""))
+        pdf(paste("MeanMSE+", "Mu=", Mu, "+b=", b, "+s=", s, "+FaB=", FaB, ".pdf", sep = ""))
         G <-
           ggplot(data = Df[Df$TrueMean == Mu &
                              # Create the plot for a given combinatio of parameters,
                              Df$Bias == b &
                              Df$BiasStrength == s &
-                             Df$FaB == F1, ]) +
+                             Df$FaB == FaB, ]) +
+          geom_tile(aes(
+            x = Hyperbole,
+            y = ExtermityBias,
+            fill = BeliefMean
+          ),
+          colour = "white") +
+          theme_minimal() +
+          scale_fill_gradient2(
+            limits = c(-3, 3),
+            name = "MSE",
+            low = "black",
+            mid = "white",
+            high = "black",
+            midpoint = 0
+          ) +
+          ggtitle(bquote(
+            paste(
+              mu,
+              " = ",
+              .(Mu),
+              ", b = ",
+              .(b),
+              ", s = ",
+              .(s),
+              ", FaB = ",
+              .(FaB),
+              sep = " "
+            )
+          )) +
+          labs(x = "h", y = "e") +
+          scale_x_continuous(breaks = seq(1, 3, 1)) +
+          scale_y_continuous(breaks = seq(0, 3, 1)) +
+          theme(
+            plot.title = element_text(
+              hjust = 0.5,
+              margin = margin(b = 10, unit = "pt"),
+              lineheight = 1.15
+            ),
+            axis.title.x =  element_text(margin = margin(t = 15, unit = "pt")),
+            axis.title.y =  element_text(margin = margin(r = 15, unit = "pt")),
+            text = element_text(size = 16)
+          )
+        print(G) # Print the plot to the file
+        dev.off() # Save and close the pdf file
+      }
+    }
+  }
+}
+
+# Plot MSE for variances for each combinations of parameters
+for (i in 1:length(TrueStateMeanVEC)) {
+  Mu <- TrueStateMeanVEC[i]
+  for (j in 1:length(BiasVEC)) {
+    b <- BiasVEC[j]
+    for (k in 1:length(BiasStrengthVEC)) {
+      s <- BiasStrengthVEC[k]
+      for (l in 1:length(FairAndBalancedVEC)) {
+        FaB <- FairAndBalancedVEC[l]
+        # Having set a given combinatio of parameters,
+        # open a PDF in which to save the plot
+        pdf(paste("VarMSE+", "Mu=", Mu, "+b=", b, "+s=", s, "+FaB=", FaB, ".pdf", sep = ""))
+        G <-
+          ggplot(data = Df[Df$TrueMean == Mu &
+                             # Create the plot for a given combinatio of parameters,
+                             Df$Bias == b &
+                             Df$BiasStrength == s &
+                             Df$FaB == FaB, ]) +
           geom_tile(aes(
             x = Hyperbole,
             y = ExtermityBias,
